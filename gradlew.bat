10d7701ad8b9c8fbb1933567c226872386717188
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-09-13 18:34:47 +0530

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,3 +1,10 @@
 @echo off
 set DIR=%~dp0
-"%DIR%gradle\wrapper\gradle-wrapper.jar" %*
+
+if exist "%JAVA_HOME%\bin\java.exe" (
+  set JAVA_EXE=%JAVA_HOME%\bin\java.exe
+) else (
+  set JAVA_EXE=java
+)
+
+"%JAVA_EXE%" -Xmx64m -Xms64m -cp "%DIR%gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
