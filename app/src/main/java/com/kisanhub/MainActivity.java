4c00b74b242a8f57bc2fb68133b76529cc22608d
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-09-14 08:50:43 +0530

    Create MainActivity.java

---
+++ app/src/main/java/com/kisanhub/MainActivity.java
@@ -0,0 +1,12 @@
+package com.kisanhub;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // React Native view attach karna yaha hoga (agar RN use karte ho)
+    }
+}
