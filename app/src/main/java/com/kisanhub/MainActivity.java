1cbaf3ea42fbc8f5877603062db7a24d328cdf46
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-09-14 09:15:05 +0530

    Update MainActivity.java

--- app/src/main/java/com/kisanhub/MainActivity.java
+++ app/src/main/java/com/kisanhub/MainActivity.java
@@ -1,18 +1,12 @@
 package com.kisanhub;
 
 import android.os.Bundle;
-import com.facebook.react.ReactActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends ReactActivity {
-
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(null); // RN apps ke liye null dena zaruri hota hai
-    }
-
-    @Override
-    protected String getMainComponentName() {
-        // Yeh name React Native app ko batata hai ki kaunsa component load karna hai
-        return "KisanHub";
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // Ye UI file load karega
     }
 }
